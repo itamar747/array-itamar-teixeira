38eb4f4170c7dc236440dcac571f862c7ffec94e
itamar <itamarteixeirasousa747@gmail.com>
2025-09-01 20:18:12 -0300

    Add files via upload

---
+++ array.py
@@ -0,0 +1,8 @@
+#LISTA DE COLEGAS
+amigos_de_sala = ["itamar", "ariel", "cristian", "mauricio"]
+# acessando o primeiro nome
+print("primeiro_nome: ", amigos_de_sala[0])
+# acessando o terceiro nome
+print("terceiro_nome: ", amigos_de_sala[2])
+# acessando o ultimo nome
+print("ultimo_nome: ", amigos_de_sala[-1])
